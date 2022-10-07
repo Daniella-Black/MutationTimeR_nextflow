069a372952848c51bfb087ec6ba644493576b782
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-10-07 14:27:56 +0100

    Update CloudOS_MTR_input_script.R

--- bin/CloudOS_MTR_input_script.R
+++ bin/CloudOS_MTR_input_script.R
@@ -25,12 +25,12 @@ vcf <- addMutTime(vcf, mt$V)
 head(mt$T)
 mcols(bb) <- cbind(mcols(bb),mt$T)
 
-pdf(file=paste0(sample, '_vaf_expected_vaf.pdf'), height=7.5, width=7.8)
+pdf(file=paste0(sample, '/', sample, '_vaf_expected_vaf.pdf'), height=7.5, width=7.8)
 plotSample(vcf,bb)
 dev.off()
 
 mt$T <- mt$T[-c(1)]
 
-write.csv(mt$T, paste0(sample,'_mT.csv'))
-write.csv(mt$V, paste0(sample,'_mV.csv'))
-write.csv(mt$V$CLS, paste0(sample,'_CLS.csv'))
+write.csv(mt$T, paste0(sample, '/',sample,'_mT.csv'))
+write.csv(mt$V, paste0(sample, '/',sample,'_mV.csv'))
+write.csv(mt$V$CLS, paste0(sample, '/',sample,'_CLS.csv'))
