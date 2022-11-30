a48854957bd23806c4fb4ddd8d14436d55733119
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-11-30 11:31:03 +0000

    Update CloudOS_MTR_input_script.R

--- bin/CloudOS_MTR_input_script.R
+++ bin/CloudOS_MTR_input_script.R
@@ -1,36 +1,90 @@
 #!/usr/local/bin/Rscript
-library(VariantAnnotation)
-library(MutationTimeR)
-library(mg14)
+library(signature.tools.lib)
+#library(MutationTimeR)
+#library(mg14)
 args = commandArgs(trailingOnly=TRUE)
 
 
-sample <- args[1]
-cnvpath <- args[2]
-vcfpath <- args[3]
+sampleID <- args[1]
+vcfpath <- args[2]
+header <- args[3]
+  
 
-callsBattenberg <- read.csv(cnvpath, sep='\t')
-#write.csv(callsBattenberg, paste0(sample, '_mT.csv'))
+genome.v="hg38"
+PR_threshold=8
+# outdir
+organ <- 'Breast'
 
-bb <- makeGRangesFromDataFrame(callsBattenberg, keep.extra.columns=TRUE)
-bb
+smallvariants_VCF <- VariantAnnotation::readVcf(vcfpath,genome = genome.v)
+e.vcf <- VariantAnnotation::expand(smallvariants_VCF)
 
-vcf <- readVcf(vcfpath)
-mt <- mutationTime(vcf, bb, n.boot=100)
+# separate SNV and Indels
+rd <- SummarizedExperiment::rowRanges(e.vcf)
+e.snv <- e.vcf[nchar(as.character(rd$REF))==1 & nchar(as.character(rd$ALT))==1,]
+selected_snv <- VariantAnnotation::fixed(e.snv)[,"FILTER"]=="PASS" & as.character(SummarizedExperiment::seqnames(e.snv)) %in% paste0("chr",c(1:22,"X","Y"))
+e.snv <- e.snv[selected_snv,]
 
-#######
-head(mt$V)
-table(mt$V$CLS)
-vcf <- addMutTime(vcf, mt$V)
-head(mt$T)
-mcols(bb) <- cbind(mcols(bb),mt$T)
 
-pdf(file=paste0(sample, '_vaf_expected_vaf.pdf'), height=7.5, width=7.8)
-plotSample(vcf,bb)
-dev.off()
+# collect SNV data into a table
+rd <- SummarizedExperiment::rowRanges(e.snv)
+rgs <- IRanges::ranges(e.snv)
+snvtab <- data.frame(chr=as.character(SummarizedExperiment::seqnames(e.snv)),
+                     POS=BiocGenerics::start(rgs),
+                     REF=as.character(rd$REF),
+                     ALT=as.character(rd$ALT),
+                     FILTER = VariantAnnotation::fixed(e.snv)[,"FILTER"],
+                     #VAF=VariantAnnotation::info(e.snv)[,"VAF"],
+                     DP=VariantAnnotation::geno(e.snv)$DP,
+                     AU=VariantAnnotation::geno(e.snv)$AU,
+                     TU=VariantAnnotation::geno(e.snv)$TU,
+                     CU=VariantAnnotation::geno(e.snv)$CU,
+                     GU=VariantAnnotation::geno(e.snv)$GU)
 
-mt$T <- mt$T[-c(1)]
+##makes the index column an ID column
+snvtab <- cbind(ID = rownames(snvtab), snvtab)
+rownames(snvtab) <- 1:nrow(snvtab)
 
-write.csv(mt$T, paste0(sample,'_mT.csv'))
-write.csv(mt$V, paste0sample,'_mV.csv'))
-write.csv(mt$V$CLS, paste0(sample,'_CLS.csv'))
+snvtab$chr = gsub("chr", "", snvtab$chr)
+sampleID_cols <- gsub("-", ".", sampleID)
+snvtab = subset(snvtab, select = -c(9,11,13,15) )
+names(snvtab)[names(snvtab) == sampleID_cols]<- "DP"
+names(snvtab)[names(snvtab) == paste("AU.", sampleID_cols, ".1",sep='')] <- "A"
+names(snvtab)[names(snvtab) == paste("TU.", sampleID_cols, ".1", sep='')] <- "T"
+names(snvtab)[names(snvtab) == paste("CU.", sampleID_cols, ".1", sep='')] <- "C"
+names(snvtab)[names(snvtab) == paste("GU.", sampleID_cols,  ".1", sep='')] <- "G"
+names(snvtab)[names(snvtab) == 'chr'] <- '#CHROM'
+
+snvtab['AD_REF'] <- rep(0, nrow(snvtab))
+snvtab['AD_ALT'] <- rep(0, nrow(snvtab))
+snvtab['FORMAT'] <- rep('DP:AD', nrow(snvtab))
+for(i in 1:2){
+  cols = c('QUAL', 'INFO')
+  i = cols[i]
+  snvtab[i] <- rep('.', nrow(snvtab))
+}
+
+
+tumour_list<- c()
+
+for (row in 1:nrow(snvtab)){
+  bases <- c('A', 'T', 'C', 'G')
+  for(nuc in 1:4){
+    nuc <- bases[nuc]
+    if(snvtab$REF[row] == nuc){
+      snvtab$AD_REF[row] <-  snvtab$AD_REF[row] + as.integer(snvtab[row, nuc])
+    }
+    else if(snvtab$ALT[row] == nuc){
+      snvtab$AD_ALT[row] <- snvtab$AD_ALT[row] +  as.integer(snvtab[row, nuc])
+    }
+  }
+  tumour_list <- append(tumour_list, paste(snvtab$DP[row], ':', snvtab$AD_REF[row], ',', snvtab$AD_ALT[row], sep=''))
+    }
+
+snvtab['TUMOR'] <- tumour_list
+
+snvtab <- snvtab[ , -which(names(snvtab) %in% c("DP","A", "T", "G", "C", "AD_REF", "AD_ALT"))]
+
+col_order <- c("#CHROM", "POS", "ID", "REF","ALT", "QUAL", "FILTER", "INFO","FORMAT" , 'TUMOR' )
+snvtab <- snvtab[, col_order]
+write.table(header, file = paste0(sampleID,"_SNVs.txt"),row.names = F,quote = F,sep = "\t", col.names=F)
+write.table(snvtab,file = paste0(sampleID,"_SNVs.txt"),sep = "\t",quote = F,col.names = T,row.names = F, append=T)
