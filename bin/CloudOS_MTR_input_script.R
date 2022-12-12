57281e2acaca9cf1561b6daa67d314cb32a22d3d
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-12-12 10:24:26 +0000

    Update CloudOS_MTR_input_script.R

--- bin/CloudOS_MTR_input_script.R
+++ bin/CloudOS_MTR_input_script.R
@@ -14,7 +14,7 @@ genome.v="hg38"
 PR_threshold=8
 # outdir
 organ <- 'Breast'
-
+header <- read.csv(header, sep='\n', header=FALSE)
 smallvariants_VCF <- VariantAnnotation::readVcf(vcfpath,genome = genome.v)
 e.vcf <- VariantAnnotation::expand(smallvariants_VCF)
 
