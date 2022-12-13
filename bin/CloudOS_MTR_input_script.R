71c87943553e8ddc943961737cf98b7544c64c6e
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-12-13 11:06:17 +0000

    Update CloudOS_MTR_input_script.R

--- bin/CloudOS_MTR_input_script.R
+++ bin/CloudOS_MTR_input_script.R
@@ -68,5 +68,5 @@ for (row in 1:nrow(snvtab)){
 snvtab['VAF'] <- snvtab['AD_ALT']/(snvtab['AD_ALT'] + snvtab['AD_REF'])
 
 pdf(file = paste0(sampleID, '_vaf_hist_all_muts.pdf'))
-hist(as.numeric(snvtab['VAF']))
+hist(as.numeric(unlist(snvtab['VAF'])))
 dev.off()
