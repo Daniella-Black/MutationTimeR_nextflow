e849b101842269423cdcde84707d27990004687c
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-03-12 14:45:31 +0000

    Update CloudOS_MTR_input_script.R

--- bin/CloudOS_MTR_input_script.R
+++ bin/CloudOS_MTR_input_script.R
@@ -132,7 +132,12 @@ sv_df$start = sv_df$start +1
 sv_df$width = sv_df$end - sv_df$start
 sv_df$major_cn = sv_df$total.copy.number.inTumour - sv_df$minor_cn
 sv_df['strand'] <- rep('*', nrow(sv_df))
-sv_df['clonal_frequency'] <- rep(as.numeric(tp)/100, nrow(sv_df))
+if(tp != 'NaN'){
+  sv_df['clonal_frequency'] <- rep(as.numeric(tp)/100, nrow(sv_df))
+  }
+else{
+  sv_df['clonal_frequency'] <- rep('NaN', nrow(sv_df))
+  }
 sv_df <- sv_df[ , -which(names(sv_df) %in% c("total.copy.number.inTumour"))]
 col_order <- c('seqnames', 'start', 'end', 'width', 'strand', 'major_cn', 'minor_cn', 'clonal_frequency')
 sv_df <- sv_df[, col_order]
